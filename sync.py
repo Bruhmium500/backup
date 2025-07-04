40c164660507afb1902306d6b71a99832c888334
Bruhmium500 <githubaction1@outlook.com>
2025-07-04 23:31:28 +0600

    Update sync.py

--- sync.py
+++ sync.py
@@ -18,7 +18,7 @@ creds = service_account.Credentials.from_service_account_info(
 drive = build("drive", "v3", credentials=creds)
 
 # static values
-BACKUP_FOLDER_ID = "1OyWrHqFI3IrCbjV-Bkh_qC3XfYohXh-D"
+BACKUP_FOLDER_ID = "14FRN9M0TqeXPwYZDuRRQkMnpF4VaTDw_"
 EXTRACT_DIR      = "/tmp/extracted_backups"
 TMP_ZIP_DIR      = "/tmp/zips"
 REPO_ID          = "testdeep123/image"
